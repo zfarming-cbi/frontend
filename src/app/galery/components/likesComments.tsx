9d3949e52b3ced4fda8b80f397fb08067c3c3118
david0497 <drave31@gmail.com>
2023-09-23 01:33:54 -0500

    refactor: refactor galery screen

---
+++ src/app/galery/components/likesComments.tsx
@@ -0,0 +1,43 @@
+import * as React from "react"
+import IconButton from "@mui/material/IconButton"
+import Stack from "@mui/material/Stack"
+import { Comment, ThumbUp } from "@mui/icons-material"
+import { Typography } from "@mui/material"
+
+interface Props extends React.PropsWithChildren {
+  likes: number
+  comments: number
+  disabled: boolean
+}
+
+export const LikesComments: React.FC<Props> = (props) => {
+  const { likes, comments, disabled } = props
+  return (
+    <Stack direction="row" spacing={1} sx={{ display: "flex", pl: 1 }}>
+      <IconButton disabled={disabled}>
+        <ThumbUp />
+      </IconButton>
+      <Typography
+        fontWeight="ligth"
+        fontSize={15}
+        color={"black"}
+        alignSelf={"center"}
+      >
+        {" "}
+        {likes}
+      </Typography>
+      <IconButton disabled={disabled}>
+        <Comment />
+      </IconButton>
+      <Typography
+        fontWeight="ligth"
+        fontSize={15}
+        color={"black"}
+        alignSelf={"center"}
+      >
+        {" "}
+        {comments}
+      </Typography>
+    </Stack>
+  )
+}
--- src/app/galery/galery.screen.tsx
+++ src/app/galery/galery.screen.tsx
@@ -11,17 +11,14 @@ import {
   TextField,
   Typography,
 } from "@mui/material"
-import {
-  Comment,
-  FirstPage,
-  LastPage,
-  Search,
-  ThumbUp,
-} from "@mui/icons-material"
+import { FirstPage, LastPage, Search } from "@mui/icons-material"
 import { useNavigate } from "react-router-dom"
 import { ROUTE_PATH } from "../../settings/routes/routes"
 import LOGO_FULL from "../../assets/placeholder.png"
 import { useGetPlantsForGaleryQuery } from "../../settings/api/endpoints/plant"
+import { LikesComments } from "./components/likesComments"
+import { DateTime } from "luxon"
+import { Remarkable } from "remarkable"
 
 export interface PlantListRow {
   id?: string | number
@@ -37,10 +34,13 @@ export const GaleryScreen: React.FC = () => {
   const navigate = useNavigate()
   const { data, isLoading, error } = useGetPlantsForGaleryQuery({
     page: "1",
-    perPage: "2",
+    perPage: "10",
   })
+  const isLogged = localStorage.getItem("token") ?? undefined
 
-  const plants2 = React.useMemo(() => {
+  const remarkable = new Remarkable()
+
+  const plants = React.useMemo(() => {
     return (
       data?.map<PlantListRow>(
         ({ id, name, likes, comments, growing_time, content, image }) => ({
@@ -49,86 +49,12 @@ export const GaleryScreen: React.FC = () => {
           likes: likes?.length ?? 0,
           comments: comments?.length ?? 0,
           growing_time,
-          content,
+          content: remarkable.render(content),
           image: image ?? "",
         })
       ) ?? []
     )
   }, [data])
-  const plants = [
-    {
-      name: "Tomate",
-      likes: 2,
-      comments: 5,
-      growing_time: "2023/10/04",
-      content:
-        "Los tomates son una planta popular en la huerta debido a su versatilidad en la cocina. Pueden ser cultivados en macetas o en el suelo y requieren un sustrato bien drenado. Asegúrate de proporcionar suficiente luz solar y riego constante para obtener una cosecha saludable. Los tomates suelen tardar alrededor de 70-90 días en madurar después de la siembra.",
-      image: "src/assets/tomates.jpg",
-    },
-    {
-      name: "Cilantro",
-      image: "src/assets/cilantro.jpg",
-      likes: 4,
-      comments: 3,
-      growing_time: "2023/10/04",
-      content:
-        "El cilantro es una hierba aromática ampliamente utilizada en diversas cocinas. Se puede cultivar en macetas o en el suelo, y prefiere la sombra parcial y un sustrato rico en nutrientes. Sus hojas son comestibles y se pueden cosechar cuando la planta alcanza una altura suficiente. El cilantro es conocido por su sabor único y fresco.",
-    },
-    {
-      name: "Repollo",
-      image: "src/assets/repollo.jpg",
-      likes: 5,
-      comments: 2,
-      growing_time: "2023/10/04",
-      content:
-        "El repollo es una verdura de hoja que crece en cabezas compactas. Es una planta resistente y puede tolerar condiciones de frío. Se debe plantar en sustrato bien drenado y enriquecido con materia orgánica. La madurez del repollo se alcanza generalmente en 70-100 días después de la siembra, dependiendo de la variedad.",
-    },
-    {
-      name: "Perejil",
-      image: "src/assets/perejil.jpg",
-      likes: 1,
-      comments: 7,
-      growing_time: "2023/10/04",
-      content:
-        "El perejil es una hierba aromática ampliamente utilizada en la cocina. Se puede cultivar en macetas o en el suelo y prefiere la luz solar parcial. Sus hojas son ricas en sabor y se pueden cosechar a medida que crece. El perejil es una planta anual que se renueva cada año.",
-    },
-    {
-      name: "Limoncillo",
-      image: "src/assets/limoncillo.jpg",
-      likes: 3,
-      comments: 2,
-      growing_time: "2023/10/04",
-      content:
-        "El limoncillo es una hierba aromática conocida por su sabor cítrico. Se puede cultivar en macetas o en el suelo y prefiere pleno sol. Sus tallos se utilizan en la cocina y para hacer té. El limoncillo es fácil de cultivar y puede crecer durante todo el año en climas cálidos.",
-    },
-    {
-      name: "Algodón",
-      image: "src/assets/algodon.jpg",
-      likes: 2,
-      comments: 8,
-      growing_time: "2023/10/04",
-      content:
-        "El algodón es una planta que produce fibras utilizadas en la fabricación de textiles. Requiere un clima cálido y suelos bien drenados. El proceso de cultivo del algodón es largo y requiere cuidado constante, desde la siembra hasta la cosecha. Las fibras de algodón se obtienen de las cápsulas de las semillas maduras.",
-    },
-    {
-      name: "Aguacate",
-      image: "src/assets/aguacate.jpg",
-      likes: 3,
-      comments: 12,
-      growing_time: "2023/10/04",
-      content:
-        "El aguacate es un árbol frutal que produce una fruta rica en grasas saludables. Se debe plantar en un lugar soleado y en suelos bien drenados. El aguacate es conocido por su largo tiempo de maduración, que puede llevar varios años. Las flores del aguacate son hermafroditas y requieren polinización para la producción de frutas.",
-    },
-    {
-      name: "Pan de árbol",
-      image: "src/assets/pan-de-arbol.jpg",
-      likes: 2,
-      comments: 1,
-      growing_time: "2023/10/04",
-      content:
-        "El pan de árbol es una planta tropical conocida por sus frutos de sabor dulce. Se debe plantar en climas cálidos y húmedos. Los frutos maduran en 8-10 meses después de la floración. El pan de árbol es una fuente importante de carbohidratos en muchas regiones tropicales.",
-    },
-  ]
 
   return (
     <Grid
@@ -145,6 +71,7 @@ export const GaleryScreen: React.FC = () => {
             variant="contained"
             color="primary"
             onClick={() => navigate(ROUTE_PATH.Login)}
+            sx={{ display: !!isLogged ? "none" : "flex" }}
           >
             Iniciar sesión
           </Button>
@@ -185,97 +112,96 @@ export const GaleryScreen: React.FC = () => {
           width: "100%",
         }}
       >
-        {plants2.map((plant, index) => {
+        {plants.map((plant, index) => {
           const style = !index
             ? { gridRowEnd: "span 1fr", gridColumnEnd: "span 2" }
             : {}
           return (
-            // <Grid paddingTop={2} item sx={{ maxWidth: 400 }}>
             <Card
-              sx={{ display: "flex", flexDirection: "row", ...style }}
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                ...style,
+              }}
               key={index}
             >
-              <CardActionArea sx={{ display: "flex" }}>
-                <Box sx={{ display: "flex", flexDirection: "column" }}>
-                  <CardContent
-                    sx={{ display: "flex", flex: 1, flexDirection: "column" }}
-                  >
-                    <Typography
-                      fontWeight="ligth"
-                      fontSize={20}
-                      textAlign={"left"}
-                      color={"black"}
-                    >
-                      {plant.name}
-                    </Typography>
-                    <Typography
-                      fontWeight="ligth"
-                      fontSize={10}
-                      textAlign={"left"}
-                      color={"black"}
-                    >
-                      {plant.content}
-                    </Typography>
-                  </CardContent>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-between",
+                  flex: 1,
+                }}
+              >
+                <CardActionArea sx={{ display: "flex", flex: 1 }}>
                   <Box
-                    sx={{ display: "flex", alignItems: "center", pl: 1, pb: 1 }}
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "space-between",
+                      flex: 1,
+                      height: "100%",
+                    }}
                   >
-                    <Box
-                      paddingRight={2}
-                      sx={{ display: "flex", alignItems: "center" }}
+                    <CardContent
+                      sx={{
+                        display: "flex",
+                        flex: 1,
+                        flexDirection: "column",
+                        paddingTop: 0,
+                        justifyContent: "space-between",
+                        "&:last-child": {
+                          paddingBottom: 0,
+                        },
+                      }}
                     >
-                      <IconButton>
-                        <ThumbUp />
-                      </IconButton>
                       <Typography
                         fontWeight="ligth"
-                        fontSize={10}
-                        textAlign={"center"}
+                        fontSize={20}
+                        textAlign={"left"}
                         color={"black"}
                       >
-                        {" "}
-                        {plant.likes}
+                        {plant.name}
                       </Typography>
-                    </Box>
-                    <Box
-                      paddingRight={2}
-                      sx={{ display: "flex", alignItems: "center" }}
-                    >
-                      <IconButton>
-                        <Comment />
-                      </IconButton>
-                      <Typography
+                      <div
+                        dangerouslySetInnerHTML={{ __html: plant.content }}
+                      />
+                      {/* <Typography
                         fontWeight="ligth"
                         fontSize={10}
-                        textAlign={"center"}
+                        textAlign={"left"}
                         color={"black"}
+                        paddingRight={1}
                       >
-                        {" "}
-                        {plant.comments}
-                      </Typography>
-                    </Box>
-                    <Box
-                      paddingRight={2}
-                      sx={{ display: "flex", alignItems: "center" }}
-                    >
+                        {plant.content}
+                      </Typography> */}
                       <Typography
                         fontWeight="ligth"
                         fontSize={10}
-                        textAlign={"center"}
+                        textAlign={"left"}
                         color={"grey"}
+                        paddingY={1}
                       >
-                        Publicado: {plant.growing_time}
+                        Publicado:{" "}
+                        {DateTime.fromISO(plant.growing_time).toLocaleString(
+                          DateTime.DATETIME_MED
+                        )}
                       </Typography>
-                    </Box>
+                    </CardContent>
                   </Box>
-                </Box>
-                <CardMedia
-                  component="img"
-                  image={plant.image}
-                  alt="plant"
-                  sx={{ width: 150 }}
+                  <CardMedia
+                    component="img"
+                    image={plant.image}
+                    alt="plant"
+                    sx={{ width: 175, height: 200, flex: 1 }}
+                  />
+                </CardActionArea>
+                <LikesComments
+                  likes={plant.likes}
+                  comments={plant.comments}
+                  disabled={!isLogged}
                 />
-              </CardActionArea>
+              </Box>
             </Card>
           )
         })}
